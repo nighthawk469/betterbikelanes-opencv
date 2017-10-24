83009c9d7fed7d685dfc3f3bb899e6df7d48560b
nighthawk469 <nick.crawford1234@gmail.com>
2017-10-24 15:42:25 -0400

    Cleanup, deactivated twitter

--- thack2/vision/final.py
+++ thack2/vision/final.py
@@ -1,9 +1,12 @@
+"""Determine if there is car in bike lane with openCV.
+"""
+
+
 import numpy as np
 import cv2
 import requests
-from tweet import tweet
-##print("hi")
-#requests.post("http://bike-thing-server.herokuapp.com/hazard")
+#from tweet import tweet
+
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 
 face_cascade = cv2.CascadeClassifier('cars.xml')#best
@@ -14,28 +17,22 @@ face_cascade = cv2.CascadeClassifier('cars.xml')#best
 # face_cascade = cv2.CascadeClassifier('cars.xml')
 
 
-cap = cv2.VideoCapture('D:/root/libraries/vision/vid.mp4')
-
+cap = cv2.VideoCapture('vid.mp4')
 informed = False
+
 while 1:
     try:
-
-        
-        print (informed)
-        
         ret, img = cap.read()
 #         print (img.shape)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray,minNeighbors =20)#, minNeighbors =40 ,scaleFactor=1.1)#,3, 3)#, 1.3, 5)
+
         for (x,y,w,h) in faces:
-
-            
-
             mid_in = (x + x+ w)/2
             right_th = 390
             left_th = 300
+
             if left_th <= mid_in <= right_th:
-                
                 cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = img[y:y+h, x:x+w]
@@ -43,21 +40,22 @@ while 1:
                 if not informed:
                     requests.post("http://bike-thing-server.herokuapp.com/hazard")
                     informed = True
-                    tweet("There is a flying car with license plate SXP 344 on Yonge Street")
+                    #tweet("@TOPolice There is a flying car with license plate SXP 344 on Yonge Street")
+
             if mid_in <= left_th:
                 pass
                 #print ("there is car to the left of bike lane")
+
             if right_th  <= mid_in:
                 pass
                 #print ("there is car to the right of lane")
-        
-        
-        cv2.imshow('img',img)
+
+        cv2.imshow('img', img)
         k = cv2.waitKey(30) & 0xff
-        
-    except: 
+
+    except:
         break
-    
+
     if k == 27: #pressing escape
         break
 
--- thack2/vision/tweet.py
+++ thack2/vision/tweet.py
@@ -1,13 +1,15 @@
+"""Must provide authentication keys.
+"""
+
 import twitter
 
 def tweet(s):
-    api = twitter.Api(consumer_key='77h8AAr1WUk1djkhZeBukwMBz',
-                      consumer_secret='PysMgbrw9D4qhEzQXyQ0VEtjdVd3E12SYRRb9iBywTo9BRQ68E',
-                      access_token_key='194338134-VJPU8sFr2sxkQblHAedGSmbofXu9tG3gSQINlpCA',
-                      access_token_secret='ZYJCKG8RU5X37KsEmQrDcpBm5Q044Jw1CQmRJvWxGwbb1')
+    api = twitter.Api(consumer_key='',
+                      consumer_secret='',
+                      access_token_key='',
+                      access_token_secret='')
 
     print(api.VerifyCredentials())
 
-    #@TorontoPolice
     status = api.PostUpdate(s)
     print(status.text)
